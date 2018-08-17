8c434ec1de455e6b3e76a953a2db97c93de4c3b5
Keionne Derousselle <knnderousselle@gmail.com>
2018-08-17 11:21:47 -0600

    extracting a post list component

---
+++ blog/components/Posts/PostList.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      :id="'1'"
+      :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
+      :title="'Hello There'"
+      :previewText="'This is my first post!'" />
+    <PostPreview
+      :id="'2'"
+      :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
+      :title="'Hello, Again'"
+      :previewText="'This is my second post!'" />
+    <PostPreview
+      :id="'3'"
+      :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
+      :title="'Hi!'"
+      :previewText="'This is my third post!'" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
--- blog/pages/index.vue
+++ blog/pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview
-       :id="'1'"
-       :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
-       :title="'Hello There'"
-       :previewText="'This is my first post!'" />
-      <PostPreview
-       :id="'2'"
-       :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
-       :title="'Hello, Again'"
-       :previewText="'This is my second post!'" />
-      <PostPreview
-       :id="'3'"
-       :thumbnail="'https://static.interestingengineering.com/images/APRIL/sizes/code-google-app_resize_md.jpg'"
-       :title="'Hi!'"
-       :previewText="'This is my third post!'" />
-    </section>
+    <PostList />
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview'
+import PostList from '@/components/Posts/PostList'
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 }
 </script>
@@ -63,14 +47,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
