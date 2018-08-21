52beb66ee303af37727dd0970f4def694083085f
Keionne Derousselle <knnderousselle@gmail.com>
2018-08-21 17:02:45 -0600

    adding an error layout and removing the header on admin layout

---
+++ blog/layouts/admin.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style>
+*, *:before, *:after
+{
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html
+{
+  font-family: 'Open Sans', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
